2e586668c6a7af6720637149fabfaee5a4d8df22
HakazeScarlet <frost.scarlet.163@gmail.com>
2025-09-04 18:38:35 +0300

    Created migrations

---
+++ src/main/resources/db/migration/V20250904172530__create_roles_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE roles (
+    UUID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    role_name VARCHAR(64) NOT NULL UNIQUE
+);
---
+++ src/main/resources/db/migration/V20250904183334__create_users_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    full_name    VARCHAR(256) NOT NULL,
+    phone_number VARCHAR(12)  UNIQUE,
+    avatar       text,
+    role_id      uuid         NOT NULL,
+    CONSTRAINT fk_user_role FOREIGN KEY (role_id)
+        REFERENCES roles(uuid)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
