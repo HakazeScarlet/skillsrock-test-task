ac950dd41a06c3a44ee3d10c35e11f0c7a7cb08e
HakazeScarlet <frost.scarlet.163@gmail.com>
2025-09-04 23:46:02 +0300

    Got rid of redundant imports

--- src/main/java/com/github/hakazescarlet/skillsrocktesttask/user/UserController.java
+++ src/main/java/com/github/hakazescarlet/skillsrocktesttask/user/UserController.java
@@ -1,8 +1,6 @@
 package com.github.hakazescarlet.skillsrocktesttask.user;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
