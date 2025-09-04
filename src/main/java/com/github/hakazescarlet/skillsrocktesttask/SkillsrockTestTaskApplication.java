f91099389d5a3b84f4a58f605ef94e6f5f9e84b9
HakazeScarlet <frost.scarlet.163@gmail.com>
2025-09-04 17:05:49 +0300

    Initial commit

---
+++ src/main/java/com/github/hakazescarlet/skillsrocktesttask/SkillsrockTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.hakazescarlet.skillsrocktesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsrockTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillsrockTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hakazescarlet/skillsrocktesttask/SkillsrockTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hakazescarlet.skillsrocktesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsrockTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
